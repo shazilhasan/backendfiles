44850af2815ec114b277b5ebdf06b8e1840430fa
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-11-10 23:12:57 +0530

    index.js

--- index.js
+++ index.js
@@ -6,54 +6,60 @@ const studentRoutes = require('./route/studentRoutes'); // Adjust the path based
 const recruiterRoutes = require('./route/recruiterRoute');
 const reasonofRejectionRoutes = require('./route/rejectionStudentRoute');
 const cors = require('cors');
-const app = express();
 const path = require('path');
 
 // Define the absolute path for serving static files
 const absoluteUploadsPath = path.join(__dirname, 'my-project');
-app.use(express.static(absoluteUploadsPath));
+const port = process.env.PORT || 3000;
 
-// Catch-All Route for Single-Page Application (SPA)
-app.use('/*', function (req, res) {
-  res.sendFile(path.join(absoluteUploadsPath, 'index.html'));
+const app = express();
+
+// Logging middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
+  next();
 });
 
-const PORT = 3000;
-console.log('.//////////////////////////////////////////////////////////////');
-
+// Enable CORS
 app.use(cors());
 
-// Middleware
+// Parse JSON requests
 app.use(bodyParser.json());
+
+// Serve static assets from the 'my-project' directory
+app.use(express.static(absoluteUploadsPath));
+
+// Define a catch-all route for the SPA (Angular app)
+app.get('/app/*', (req, res) => {
+  res.sendFile(path.join(absoluteUploadsPath, 'index.html'));
+});
 
 // Connect to the database
 db.connect()
   .then(() => {
-    // Database connected successfully
     console.log('Database connected');
+    // Include the user routes
+    app.use('/apis/users', userRoutes);
+
+    // Include the student routes
+    app.use('/apis/students', studentRoutes);
+    app.use('/apis/recruiters', recruiterRoutes);
+    app.use('/apis/reasonofRejection', reasonofRejectionRoutes);
+
+    // Serving static assets from the 'assets' directory
+    app.use(express.static('assets'));
+
+    // Test route
+    app.get('/', (req, res) => {
+      res.send('Server is running, database is connected');
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch((error) => {
-    // Database connection failed
     console.error(error.message);
     process.exit(1);
   });
-
-// Include the user routes
-app.use('/apis/users', userRoutes);
-
-// Include the student routes
-app.use('/apis/students', studentRoutes);
-app.use('/apis/recruiters', recruiterRoutes);
-app.use('/apis/reasonofRejection', reasonofRejectionRoutes);
-
-// Serving static assets from 'assets' directory
-app.use(express.static('assets'));
-
-// Test route
-app.get('/', (req, res) => {
-  res.send('Server is running, database is connected');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
